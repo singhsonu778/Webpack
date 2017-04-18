47084b58864c9b811e407d219a6c0a2732c141c7
singhsonu778 <singhsonu778@gmail.com>
2017-04-18 20:27:48 +0530

    Add html and js files

---
+++ src/js/component.js
@@ -0,0 +1,5 @@
+var renderComponent = function () {
+    var component = document.getElementById('root');
+	component.innerHTML = 'Webpack Page';
+	document.body.appendChild(renderElement());
+};
---
+++ src/js/element.js
@@ -0,0 +1,5 @@
+var renderElement = function () {
+    var element = document.createElement('h2');
+    element.innerHTML = 'Hello World!!!';
+    return element;
+};
