6820d053d434ecb727953052393f7f91a812e818
singhsonu778 <singhsonu778@gmail.com>
2017-04-19 21:06:29 +0530

    Show usage of plugins in webpack
    This will generate a file dist/index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
 
 module.exports = {
     entry: './src/js/index.js',
@@ -13,5 +14,8 @@ module.exports = {
 				use: 'babel-loader'
 			}
 		]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/template/template.html'})
+    ]
 };
